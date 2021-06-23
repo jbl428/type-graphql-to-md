9306b9d0639a3e8bf9c07ebeab876fc6ec6adab9
Jake Son <sonjeabin@naver.com>
2021-06-24 06:42:52 +0900

    Replace functions from ramda to fp-ts's function

--- src/get-types.ts
+++ src/get-types.ts
@@ -1,6 +1,6 @@
-import { pipe, prop } from 'ramda';
 import { TypeOptions, TypeValue } from 'type-graphql/dist/decorators/types';
 import { BuildContext } from 'type-graphql/dist/schema/build-context';
+import { pipe } from 'fp-ts/function';
 
 import { getScalaTypeText } from './get-scalar-type-text';
 import { metadata } from './metadata';
@@ -12,15 +12,17 @@ const makePredicate =
   (obj: T) =>
     (obj[name] as unknown) === type;
 
-const getOtherOutputType: (type: TypeValue) => string = pipe(
-  makePredicate,
-  (predicate): { name: string } =>
-    metadata.objectTypes.find(predicate('target')) ||
-    metadata.interfaceTypes.find(predicate('target')) ||
-    metadata.enums.find(predicate('enumObj')) ||
-    metadata.unions.find(predicate('symbol')) || { name: '' },
-  prop('name')
-);
+const getOtherOutputType = (type: TypeValue): string =>
+  pipe(
+    type,
+    makePredicate,
+    (predicate) =>
+      metadata.objectTypes.find(predicate('target')) ||
+      metadata.interfaceTypes.find(predicate('target')) ||
+      metadata.enums.find(predicate('enumObj')) ||
+      metadata.unions.find(predicate('symbol')),
+    (result) => result?.name ?? ''
+  );
 
 export const getOutputType = (
   type: TypeValue,
@@ -32,13 +34,15 @@ export const getOutputType = (
     BuildContext.nullableByDefault
   );
 
-const getOtherArgType: (type: TypeValue) => string = pipe(
-  makePredicate,
-  (predicate): { name: string } =>
-    metadata.inputTypes.find(predicate('target')) ||
-    metadata.enums.find(predicate('enumObj')) || { name: '' },
-  prop('name')
-);
+const getOtherArgType = (type: TypeValue): string =>
+  pipe(
+    type,
+    makePredicate,
+    (predicate) =>
+      metadata.inputTypes.find(predicate('target')) ||
+      metadata.enums.find(predicate('enumObj')),
+    (result) => result?.name ?? ''
+  );
 
 export const getArgType = (type: TypeValue, typeOptions: TypeOptions): string =>
   wrapWithTypeOptions(
